1a89764d54c1075bb34f72c29dc9321b6bad9a95
Alex Balabon <balabon7@gmail.com>
2020-08-11 15:33:49 +0300

    Refactoring

---
+++ Networking/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  Networking
+//
+//  Created by mac on 10.08.2020.
+//  Copyright © 2020 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+    
+}
---
+++ Networking/MainViewController.swift
@@ -0,0 +1,67 @@
+//
+//  MainViewController.swift
+//  Networking
+//
+//  Created by mac on 10.08.2020.
+//  Copyright © 2020 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+enum Actions: String, CaseIterable {
+    case downloadImage = "Donwnload Image"
+    case get = "GET"
+    case post = "POST"
+    case ourCourses = "OUR Courses"
+    case uploadImage = "Upload Image"
+}
+
+private let reuseIdentifier = "Cell"
+private let url = "https://jsonplaceholder.typicode.com/posts"
+
+class MainViewController: UICollectionViewController {
+    
+   // let actions = ["Donwnload Image", "GET", "POST", "OUR Courses", "Upload Image"]
+    let actions = Actions.allCases
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: UICollectionViewDataSource
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return actions.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CollectionViewCell
+        
+        cell.label.text = actions[indexPath.row].rawValue
+        
+        return cell
+    }
+    
+    // MARK: UICollectionViewDelegate
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let action = actions[indexPath.row]
+        
+        switch action {
+        case Actions.downloadImage:
+            performSegue(withIdentifier: "ShowImage", sender: self)
+        case Actions.get:
+            NetworkManager.getRequest(url: url)
+        case Actions.post:
+            NetworkManager.postRequest(url: url)
+        case Actions.ourCourses:
+            performSegue(withIdentifier: "OurCourses", sender: nil)
+        case Actions.uploadImage:
+            print("Upload Image")
+        }
+    }
+    
+}
