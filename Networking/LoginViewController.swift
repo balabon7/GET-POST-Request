3ccc8cf86dd1d6fbba82603b309f6be3579154d8
Alex Balabon <balabon7@gmail.com>
2020-08-18 18:12:12 +0300

    Custom Facebook Login Button (doesn't work now)

--- Networking/LoginViewController.swift
+++ Networking/LoginViewController.swift
@@ -14,8 +14,20 @@ class LoginViewController: UIViewController {
 
     lazy var loginButton: UIButton = {
         let button = FBLoginButton()
-        button.frame = CGRect(x: 32, y: 320, width: view.frame.width - 64, height: 50)
+        button.frame = CGRect(x: 32, y: 360, width: view.frame.width - 64, height: 50)
         button.delegate = self
+        return button
+    }()
+    
+    lazy var customFBLoginButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor(hexValue: "#3B5999", alpha: 1)
+        button.setTitle("Login with Facebook", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.setTitleColor(.white, for: .normal)
+        button.frame = CGRect(x: 32, y: 360 + 60, width: view.frame.width - 64, height: 50)
+        button.layer.cornerRadius = 4
+        button.addTarget(self, action: #selector(handleCastomLogin), for: .touchUpInside)
         return button
     }()
     
@@ -24,13 +36,11 @@ class LoginViewController: UIViewController {
 
         setupViews()
     }
-
-        
     
     private func setupViews() {
         view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
-       // loginButton.center = view.center
         view.addSubview(loginButton)
+        view.addSubview(customFBLoginButton)
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -63,6 +73,11 @@ extension LoginViewController: LoginButtonDelegate {
     
     private func openMainViewController() {
         dismiss(animated: true)
+    }
+    
+    @objc private func handleCastomLogin() {
+        
+        print("Custom login button pressed")
     }
     
 }
